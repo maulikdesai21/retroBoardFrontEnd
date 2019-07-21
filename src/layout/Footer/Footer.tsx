214f7cd2eeaa8d6aacc7984e37de7f508c1ef916
Desai, Maulik (CORP) <Maulik.Desai@adp.com>
2019-07-20 21:36:45 -0400

    Adding Header and Footer

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./Footer.scss";
 export const Footer = () => {
   return (
-    <h1>Hey Footer</h1>
+    <div className="footerDisclosure">
+      Agile Retro Board by <a href="https://github.com/maulikdesai21"> Maulik Desai </a>. The source code is licensed  MIT
+    </div>
   )
 }
